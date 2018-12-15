132814754ea5cf0a057224910ca769a30c5e4c30
DinoLai <dinos80152@gmail.com>
2018-12-15 21:58:26 +0800

    add unit test example

---
+++ test_example.py
@@ -0,0 +1,13 @@
+from example import World
+import unittest
+
+class TestWorld(unittest.TestCase):
+
+    def setUp(self):
+        self.world = World()
+
+    def test_welcome(self):
+        self.assertEqual('hello world', self.world.welcome())
+
+if __name__ == '__main__':
+   unittest.main()
