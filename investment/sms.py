79a7b41d05d88a57462e0aa65dd00cd781ac050d
Jim Chen <jjchen1023@gmail.com>
2018-12-22 13:55:04 +0800

    SMS notify
    SMS notify

--- investment/sms.py
+++ investment/sms.py
@@ -1,14 +1,14 @@
+import os
 import boto3
-from config import ACCESS_KEY, SECRET_KEY
 
 class SMS():
     def __init__(self):
         self.sns = boto3.client('sns', 
-                                region_name='eu-west-1',
-                                aws_access_key_id=ACCESS_KEY,
-                                aws_secret_access_key=SECRET_KEY)   
+                                region_name=os.getenv('AWS_REGIN'),
+                                aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+                                aws_secret_access_key=os.getenv('AWS_SECRET_KEY'))   
     
-    def send(self, phone='+886912078059', msg=''):
+    def send(self, phone=os.getenv('PHONE'), msg=''):
         print('send SMS to {0}'.format(phone))
         self.sns.publish(PhoneNumber = phone,
                          Message = msg)
