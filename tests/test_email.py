2c34a81e60164f91ed9236da1559a0d34c4c4945
DinoLai <dinos80152@gmail.com>
2018-12-22 14:34:46 +0800

    add email class

---
+++ tests/test_email.py
@@ -0,0 +1,11 @@
+from investment.email import EMail
+import unittest
+
+class TestEMail(unittest.TestCase):
+
+    def test_send(self):
+        email = EMail()
+        email.send(["dinos80152@gmail.com"], "test", "test")
+
+if __name__ == '__main__':
+   unittest.main()
